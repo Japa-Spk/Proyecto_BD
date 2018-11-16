2487a9bf51dc8131ac653a6b3fea7b3d6f92924d
JOSE <JOSE@DESKTOP-P30MQQU>
2018-11-16 00:47:07 -0500

    Clases Basicas Cliente y Producto.

--- Proyecto_BD/src/Modelo/Producto.java
+++ Proyecto_BD/src/Modelo/Producto.java
@@ -42,6 +42,134 @@ public class Producto {
 		this.nombre = nombre;
 		this.valorUnitario = valorUnitario;
 	}
+
+
+
+	public String toString() {
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("Codigo de producto: ").append(codigo).append(", ");
+			sb.append("Nombre de producto: ").append(nombre).append(", "); 
+			sb.append("Valor Unitario: ").append(valorUnitario).append(", ");
+			
+			return sb.toString();	
+	
+	}
+
+	
+	
+	
+	
+	
+	
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+	/**
+	 * @return el codigo
+	 */
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * @param codigo el codigo a establecer
+	 */
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * @return el nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * @param nombre el nombre a establecer
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * @return el valorUnitario
+	 */
+	public int getValorUnitario() {
+		return valorUnitario;
+	}
+
+
+
+
+
+
+
+
+
+
+	/**
+	 * @param valorUnitario el valorUnitario a establecer
+	 */
+	public void setValorUnitario(int valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
