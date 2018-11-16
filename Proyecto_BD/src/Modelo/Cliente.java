c49e4a8317f03911be1464cc314af4399bd5fd2b
JOSE <japinto08@ucatolica.edu.co>
2018-11-16 00:44:06 -0500

    Clases Base Cliente Y Producto.

--- Proyecto_BD/src/Modelo/Cliente.java
+++ Proyecto_BD/src/Modelo/Cliente.java
@@ -4,9 +4,110 @@
 package Modelo;
 
 /**
- * @author JOSE
+ * @author Jose , Bryan
  *
  */
 public class Cliente {
+	private int cedula;
+	private String nombre;
+	
+	/**Constructor Normal
+	/**
+	 * @param cedula
+	 * @param nombre
+	 */
+	public Cliente() {
+		cedula = 0;
+		nombre = "Cliente";
+	}
 
+	
+	
+	
+	/** CONSTRUCTOR POR PARAMETROS DE CLASE CLIENTE
+	 * @param cedula
+	 * @param nombre
+	 */
+	public Cliente(int cedula, String nombre) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+	}
+
+
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Cedula del titular: ").append(cedula).append("\r\n");
+		sb.append("Nombre del cliente").append(nombre).append("\r\n");
+		return sb.toString();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	/**
+	 * @return el cedula
+	 */
+	public int getCedula() {
+		return cedula;
+	}
+
+
+
+
+	/**
+	 * @param cedula el cedula a establecer
+	 */
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
+
+
+
+	/**
+	 * @return el nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+	/**
+	 * @param nombre el nombre a establecer
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ Proyecto_BD/src/Modelo/Producto.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package Modelo;
+
+/**
+ * @author JOSE
+ *
+ */
+public class Producto {
+	private int codigo;
+	private String nombre;
+	private int valorUnitario;
+	
+	
+	
+	
+	public Producto() {
+		codigo=0;
+		nombre="Producto";
+		valorUnitario=0;
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**CONSTRUCTOR POR PARAMETROS DE LA CLASE PRODUCTO
+	 * @param codigo
+	 * @param nombre
+	 * @param valorUnitario
+	 */
+	public Producto(int codigo, String nombre, int valorUnitario) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.valorUnitario = valorUnitario;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
