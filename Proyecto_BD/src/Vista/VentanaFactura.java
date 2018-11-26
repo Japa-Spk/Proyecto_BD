fba6007150d2868d5f4632ed17cac6ed9aa764aa
IKAROS <IKAROS@LAPTOP-640M0PMQ>
2018-11-25 19:50:43 -0500

    Getters and setters from VentanaFactura

--- Proyecto_BD/src/Vista/VentanaFactura.java
+++ Proyecto_BD/src/Vista/VentanaFactura.java
@@ -433,4 +433,285 @@ public class VentanaFactura extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JButton primero;
     // End of variables declaration//GEN-END:variables
+	/**
+	 * @return the actualizar
+	 */
+	public javax.swing.JButton getActualizar() {
+		return Actualizar;
+	}
+
+	/**
+	 * @param actualizar the actualizar to set
+	 */
+	public void setActualizar(javax.swing.JButton actualizar) {
+		Actualizar = actualizar;
+	}
+
+	/**
+	 * @return the buscar
+	 */
+	public javax.swing.JButton getBuscar() {
+		return Buscar;
+	}
+
+	/**
+	 * @param buscar the buscar to set
+	 */
+	public void setBuscar(javax.swing.JButton buscar) {
+		Buscar = buscar;
+	}
+
+	/**
+	 * @return the consultas
+	 */
+	public javax.swing.JButton getConsultas() {
+		return Consultas;
+	}
+
+	/**
+	 * @param consultas the consultas to set
+	 */
+	public void setConsultas(javax.swing.JButton consultas) {
+		Consultas = consultas;
+	}
+
+	/**
+	 * @return the descuento
+	 */
+	public javax.swing.JTextField getDescuento() {
+		return Descuento;
+	}
+
+	/**
+	 * @param descuento the descuento to set
+	 */
+	public void setDescuento(javax.swing.JTextField descuento) {
+		Descuento = descuento;
+	}
+
+	/**
+	 * @return the eliminar
+	 */
+	public javax.swing.JButton getEliminar() {
+		return Eliminar;
+	}
+
+	/**
+	 * @param eliminar the eliminar to set
+	 */
+	public void setEliminar(javax.swing.JButton eliminar) {
+		Eliminar = eliminar;
+	}
+
+	/**
+	 * @return the factura_titulo
+	 */
+	public javax.swing.JLabel getFactura_titulo() {
+		return Factura_titulo;
+	}
+
+	/**
+	 * @param factura_titulo the factura_titulo to set
+	 */
+	public void setFactura_titulo(javax.swing.JLabel factura_titulo) {
+		Factura_titulo = factura_titulo;
+	}
+
+	/**
+	 * @return the fecha
+	 */
+	public javax.swing.JTextField getFecha() {
+		return Fecha;
+	}
+
+	/**
+	 * @param fecha the fecha to set
+	 */
+	public void setFecha(javax.swing.JTextField fecha) {
+		Fecha = fecha;
+	}
+
+	/**
+	 * @return the grabar
+	 */
+	public javax.swing.JButton getGrabar() {
+		return Grabar;
+	}
+
+	/**
+	 * @param grabar the grabar to set
+	 */
+	public void setGrabar(javax.swing.JButton grabar) {
+		Grabar = grabar;
+	}
+
+	/**
+	 * @return the iVA
+	 */
+	public javax.swing.JTextField getIVA() {
+		return IVA;
+	}
+
+	/**
+	 * @param iVA the iVA to set
+	 */
+	public void setIVA(javax.swing.JTextField iVA) {
+		IVA = iVA;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public javax.swing.JTextField getNombre() {
+		return Nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(javax.swing.JTextField nombre) {
+		Nombre = nombre;
+	}
+
+	/**
+	 * @return the numero
+	 */
+	public javax.swing.JTextField getNumero() {
+		return Numero;
+	}
+
+	/**
+	 * @param numero the numero to set
+	 */
+	public void setNumero(javax.swing.JTextField numero) {
+		Numero = numero;
+	}
+
+	/**
+	 * @return the productos
+	 */
+	public javax.swing.JTable getProductos() {
+		return Productos;
+	}
+
+	/**
+	 * @param productos the productos to set
+	 */
+	public void setProductos(javax.swing.JTable productos) {
+		Productos = productos;
+	}
+
+	/**
+	 * @return the siguiente
+	 */
+	public javax.swing.JButton getSiguiente() {
+		return Siguiente;
+	}
+
+	/**
+	 * @param siguiente the siguiente to set
+	 */
+	public void setSiguiente(javax.swing.JButton siguiente) {
+		Siguiente = siguiente;
+	}
+
+	/**
+	 * @return the subTotal
+	 */
+	public javax.swing.JTextField getSubTotal() {
+		return SubTotal;
+	}
+
+	/**
+	 * @param subTotal the subTotal to set
+	 */
+	public void setSubTotal(javax.swing.JTextField subTotal) {
+		SubTotal = subTotal;
+	}
+
+	/**
+	 * @return the total
+	 */
+	public javax.swing.JTextField getTotal() {
+		return Total;
+	}
+
+	/**
+	 * @param total the total to set
+	 */
+	public void setTotal(javax.swing.JTextField total) {
+		Total = total;
+	}
+
+	/**
+	 * @return the ultimo
+	 */
+	public javax.swing.JButton getUltimo() {
+		return Ultimo;
+	}
+
+	/**
+	 * @param ultimo the ultimo to set
+	 */
+	public void setUltimo(javax.swing.JButton ultimo) {
+		Ultimo = ultimo;
+	}
+
+	/**
+	 * @return the valDescuento
+	 */
+	public javax.swing.JTextField getValDescuento() {
+		return ValDescuento;
+	}
+
+	/**
+	 * @param valDescuento the valDescuento to set
+	 */
+	public void setValDescuento(javax.swing.JTextField valDescuento) {
+		ValDescuento = valDescuento;
+	}
+
+	/**
+	 * @return the anterior
+	 */
+	public javax.swing.JButton getAnterior() {
+		return anterior;
+	}
+
+	/**
+	 * @param anterior the anterior to set
+	 */
+	public void setAnterior(javax.swing.JButton anterior) {
+		this.anterior = anterior;
+	}
+
+	/**
+	 * @return the cedula
+	 */
+	public javax.swing.JTextField getCedula() {
+		return cedula;
+	}
+
+	/**
+	 * @param cedula the cedula to set
+	 */
+	public void setCedula(javax.swing.JTextField cedula) {
+		this.cedula = cedula;
+	}
+
+	/**
+	 * @return the primero
+	 */
+	public javax.swing.JButton getPrimero() {
+		return primero;
+	}
+
+	/**
+	 * @param primero the primero to set
+	 */
+	public void setPrimero(javax.swing.JButton primero) {
+		this.primero = primero;
+	}
+    
+    
 }
